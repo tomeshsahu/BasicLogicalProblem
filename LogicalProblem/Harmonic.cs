42918e329069157e4458ce562b0af8151196d874
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-11 18:02:49 +0530

    [TOMESHWAR]ADD:-HARMONIC NUMBER

---
+++ LogicalProblem/Harmonic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+   public class Harmonic
+    {
+            double result = 0.0;
+            public void Harmo()
+        { 
+                Console.WriteLine("Enter Number ");
+                int Num = Convert.ToInt32((Console.ReadLine()));
+
+                for (int i = 1; i <= Num; i++)
+                {
+                    result = result + (0.1) / i;
+
+                }
+            Console.WriteLine(result);
+
+            }
+        }
+    }
+
--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -3,7 +3,7 @@ using LogicalProblem;
 
 
 Console.WriteLine("Welcome to Basic Programs");
-Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.Power");
+Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.Power\n4.Harmonic");
 Console.WriteLine("Take an option to execute");
 int Option=Convert.ToInt32(Console.ReadLine());
 switch (Option)
@@ -19,6 +19,10 @@ switch (Option)
     case 3:
         Power Powr = new Power();
         Powr.Powerv();
+        break;
+    case 4:
+        Harmonic Harmo = new Harmonic();
+        Harmo.Harmo();
         break;
     default:
         Console.WriteLine("Enter proper Option to Execute");
